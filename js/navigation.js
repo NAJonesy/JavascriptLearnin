911e03d197f523db47213a3ee88747d1668ac24d
Nik Jones <nicholas.jones@dish.com>
2018-09-26 16:37:38 -0600

    Bootstrap trial page

---
+++ js/navigation.js
@@ -0,0 +1,12 @@
+
+// document.write('
+// <nav class="navBar">
+//     <ul>
+//       <li><a href="index.html">Home</a></li>
+//       <li><a href="page2.html">Page2</a></li>
+//       <li><a href="formPage.html">Form</a></li>
+//       <li><a href="login.html" class ="active">Login</a></li>
+//     </ul>
+// </nav>')
+
+document.write('<nav class="navBar"><ul><li><a href="index.html">Home</a></li><li><a href="page2.html">Page2</a></li><li><a href="formPage.html">Form</a></li><li><a href="login.html" class ="active">Login</a></li></ul></nav>')
